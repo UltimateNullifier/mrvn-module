ae7c3f2e636768b1d5d64d6ebb27b0ab7379dc3f
DMDXV <Padfootdiamond@Gmail.com>
2020-12-08 15:30:17 +0000

    V1.0.0

---
+++ index.js
@@ -0,0 +1,33 @@
+const https = require("https");
+
+exports.mrvn = {
+  post: async function (data, endpoint) {
+    data = JSON.stringify(data);
+
+    const options = {
+      hostname: "mrvn.me",
+      port: 443,
+      path: `/p/${endpoint}`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": data.length,
+      },
+    };
+
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      });
+    });
+
+    req.on("error", (error) => {
+      console.error(error);
+    });
+
+    req.write(data);
+    return req.end();
+  },
+};
